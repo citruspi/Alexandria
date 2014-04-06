cece83f39d9a972e4f40e0cafa640bfdf2b400b6
Mihir Singh <me@mihirsingh.com>
2014-04-05 21:24:34 -0400

    Moved options from debug class up to parent config class

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import os
 class Config(object):
 
     DEBUG = False
+    SECRET_KEY = os.urandom(30).encode('hex')
+    TEMP_DIR = 'tmp'
+    LIB_DIR = 'books'
+    
 
 class Debug(Config):
 
     DEBUG=True
-    SECRET_KEY = os.urandom(30).encode('hex')
-    TEMP_DIR = 'tmp'
-    LIB_DIR = 'books'
